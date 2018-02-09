b6de84c7f81dd907e8148304b422cc48f28a6313
Mazanek <gesterom@gmail.com>
2018-02-09 18:29:14 +0100

    update

--- headers/Encja.h
+++ headers/Encja.h
@@ -6,78 +6,31 @@
 template<typename Type>
 class Updater{
 	public:
-	virtual void update( Type data) = 0;
-	Updater(){
-		
-	}
+	void update(Type * data) {throw new NotImplemented();}
+	Updater(){}
 };
-
-template<>
-class Updater <int>
-{
-	std::string name;
-	public:
-	void update(int data){
-		std::cout<<"I sava one int. Value : "<<data<<" My name is : "<<name<<std::endl;
-	}
-	Updater(std::string m_name){
-		this->name = m_name;
-	}
-	Updater(){
-		this->name = "default";
-	}
-};
-
 template<typename Type>
 class Loader{
 	public:
 	Type * load(){throw new NotImplemented();}
-	Loader(){
-		
-	}
 };
-
-template<>
-class Loader <int>
-{
-	public:
-	std::istream& iss;
-	int * ret;
-	int * load(){
-		if(ret)
-			return ret;
-			
-		int t;
-		std::cin>>t;
-		int * ret = new int;
-		*ret = t;
-		std::cout<<"loaaaaaad...\n";
-		return ret;
-	}
-	Loader(std::istream& m_iss = std::cin) : iss(m_iss){
-		std::cout<<"load...\n";
-		ret=0;
-	}
-	Loader(int v) : iss(std::cin){
-		ret= new int;
-		*ret = v;
-	}
-};
-
 template<typename Type>
 class Encja {
 
 	private:
 	Type * data;
 	Updater<Type> * updater;
+	Loader<Type> * loader;
+	bool used = false;
 	protected:
 	public:
-		Encja(Loader<Type> * m_loader = new Loader<Type>(),Updater<Type> * m_updater = new Updater<Type>()){
-			std::cout<<"lol"<<std::endl;
-			this->data = m_loader->load();
+		Encja(Loader<Type> * m_loader,Updater<Type> * m_updater){
+			this->loader = m_loader;
 			this->updater = m_updater;
 		}
 		operator Type&(){
+			if(not used)
+				this->data = this->loader->load();
 			return *(this->data);
 		}
 		Encja( const Encja& other); // copy constructor
@@ -88,7 +41,9 @@ class Encja {
 
 	virtual ~Encja(){
 		updater->update(*data);
-		std::cout<<"samo zło"<<std::endl;
+		
+		delete updater;
+		delete loader;
 		delete data;
 	}
 
--- headers/Factory.h
+++ headers/Factory.h
@@ -14,11 +14,3 @@ class Factory
 	private:
 		/* data */
 };
-
-template<>
-Encja<int> Factory<int>::createNewReference(int v){
-	int value;
-	std::cout<<"I am factory and create int : "<<value<<std::endl;
-	Encja<int> ret;
-	return ret;
-};
