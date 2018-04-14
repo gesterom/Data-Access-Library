5430d2b4c754aaa81c98f199941bad88a3a69e67
Mazanek <gesterom@gmail.com>
2018-04-14 21:17:12 +0200

    ok

--- example/SpecLoader.h
+++ example/SpecLoader.h
@@ -2,34 +2,10 @@
 #include "../DAL/DAL.h"
 #include <fstream>
 template <typename Type>
-class Loader : public DAL::ILoader<Type> {
-  public:
-	Loader() {}
-	Loader( const Loader<Type>& other ) {
-		std::cout << "not implemented" << std::endl;
-		throw new DAL::NotImplemented( "Loader" );
-	}
-	virtual Type* load() override {
-		std::cout << "not implemented" << std::endl;
-		throw new int;
-	}
-	virtual std::string toString()override {
-		std::cout << "not implemented" << std::flush << std::endl;
-		throw new DAL::NotImplemented( "Loader toString" );
-	}
-	virtual DAL::ILoader<Type>* getCopyPtr()const {
-		std::cout << "not implemented" << std::flush << std::endl;
-		throw new DAL::NotImplemented( "ILoader =====" );
-	}
-};
+class Loader{};
 
 template <typename Type>
-class Updater: public DAL::IUpdater<Type> {
-  public:
-	Updater() {}
-	virtual void update( Type& ) {}
-	virtual DAL::IUpdater<Type>* getCopyPtr()const {}
-};
+class Updater{};
 
 template <>
 class Loader <int> : public DAL::ILoader<int> {
